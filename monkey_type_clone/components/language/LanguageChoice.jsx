a248f78a2ca15e03d4d0b5ad427b45173679b271
Tisdale <anthonytisdale@csus.edu>
2023-10-05 17:21:08 -0700

    Latest commit of ui development. Here is some edits that helps the look and feel.

--- monkey_type_clone/components/language/LanguageChoice.jsx
+++ monkey_type_clone/components/language/LanguageChoice.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { faEarthAmerica } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import './LanguageChoice.css'
 export default function LanguageChoice() {
     return (
-        <div>
-            <span><FontAwesomeIcon icon={faEarthAmerica}/> english</span>
+        <div className="logo-container">
+            <span className="logo"><FontAwesomeIcon icon={faEarthAmerica}/> english</span>
         </div>
     )
 }
