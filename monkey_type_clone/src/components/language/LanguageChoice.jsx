d945b2798842eacb8f2636f3940b9ac9a36c832e
Tisdale <anthonytisdale@csus.edu>
2023-10-12 14:46:29 -0700

    Last edit to the sizing of language prompt in tailwindcss

--- monkey_type_clone/src/components/language/LanguageChoice.jsx
+++ monkey_type_clone/src/components/language/LanguageChoice.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function LanguageChoice() {
     return (
         <div className="m-4 p-4">
-            <span className="text-lg"><FontAwesomeIcon icon={faEarthAmerica}/> english</span>
+            <span className="text-xl"><FontAwesomeIcon icon={faEarthAmerica}/> english</span>
         </div>
     )
 }
